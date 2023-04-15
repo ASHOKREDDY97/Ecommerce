65da359a3d4dc1a553c4fa414556f50dfe0f5393
Narsi12 <87262117+Narsi12@users.noreply.github.com>
2023-04-15 17:19:37 +0530

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wfy!ob!k0xlxwa!efkc!u6__ut=mufae5zn_e&8^lz2%wbpcte'
+SECRET_KEY = 'DjanoScreatKey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -93,12 +93,12 @@ DATABASES = {
 
 # DATABASES = { 
 #     'default': { 
-#     'ENGINE': 'django.db.backends.postgresql', 
+#     'ENGINE': 'DATABASE ENGINE'
 #     # 'ENGINE': 'django.contrib.gis.db.backends.postgis', 
-#     'NAME': 'omega', 
-#     'USER': 'postgres', 
-#     'PASSWORD': 'root', 
-#     'HOST': 'localhost', 
+#     'NAME': 'DB NAME', 
+#     'USER': 'DB USER', 
+#     'PASSWORD': 'DB PAssword', 
+#     'HOST': 'YOUR HOST URL', 
 #     } 
 # }
 
@@ -159,10 +159,10 @@ MESSAGE_TAGS = {
 }
 
 #SMTP configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'dajngoBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'chnarsimha2580@gmail.com'
-EMAIL_HOST_PASSWORD = 'ouyc efoi scaj ugqk'
-DEFAULT_FROM_EMAIL = 'OmegaMart<chnarsimha2580@gmail.com>'
+EMAIL_HOST_USER = 'Your Email'
+EMAIL_HOST_PASSWORD = 'Your Email App Password'
+DEFAULT_FROM_EMAIL = 'OmegaMart<Your Email>'
